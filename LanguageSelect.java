b39f6946108b2c98a2caddeb255328fa01d4f42e
dmepaiyeda <dmepaiyeda@gmail.com>
2017-04-11 14:14:39 -0400

    Update LanguageSelect.java

--- LanguageSelect.java
+++ LanguageSelect.java
@@ -1,4 +1,7 @@
-
+/*
+ * this class is for allowing the user select the language they the system to be in
+ * this is for the Language Selection use case
+ */
 public class LanguageSelect
 {
 		
@@ -18,14 +21,13 @@ public class LanguageSelect
 				 * @param Language stores the language the user selects
 				 * @return False if the user did not select the French language 
 				 */
-				boolean change (String language){//changes the language option of the user
-					//language = "English";
+				boolean change (String language){//changes the language option of the user to French
+					
 					if (language == "French")
 					{
 						//System.out.println(" french"); //testing
 						return true;
 					}
-						//System.out.println("cheece"); //testing
 						return false;
 
 				}
