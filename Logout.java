071f77e2d10a2b788f811a0f1d45788cf2373f15
pnkapadia <prachi.kapadia@hotmail.com>
2017-04-12 21:41:08 -0400

    Add files via upload

---
+++ Logout.java
@@ -0,0 +1,20 @@
+/**
+ * @(#)Logout.java
+ *
+ *
+ * @author
+ * @version 1.00 2017/4/9
+ */
+
+
+public class Logout {
+	String profile;
+
+    public boolean Logout() {
+		if(profile = true) {
+		//TODO
+		} else {
+			return false;
+		}
+    }
+}
---
+++ ReportResolve.java
@@ -0,0 +1,37 @@
+/**
+ * @(#)ReportResolve.java
+ *
+ *
+ * @author
+ * @version 1.00 2017/4/9
+ */
+
+
+public class ReportResolve {
+	String alertList[]; //contains list of notifications
+	String resolved; //stores all the resolved problems
+
+//Not sure if I should use a boolean or a String[] method
+//whichever you prefer
+    public boolean notify() { //todo
+	if(resolved = false) {
+	  System.out.println("There is a problem.");
+	} else {
+
+	}//TODO
+    }
+
+
+    public String[] notify() { //notifys problem
+	//TODO
+    }
+
+    public boolean notify_user() { //notifiys user the problem is gone
+	if(resolved = true) {	//if there's no problem, it thanks them and moves on
+	  System.out.println("The problem has been resolved. Thank you for your contribution.");
+	} else {
+	  return false;
+	}
+    }
+
+}
